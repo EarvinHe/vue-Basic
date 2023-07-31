94364e35e0076d5e341aeacb7298115eefab5b48
EarvinHe <EarvinHe@aliyun.com>
2023-07-31 13:42:07 +0800

    Vue-Basic

---
+++ 组件/单文件组件/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <School></School>
+    <student></student>
+  </div>
+</template>
+
+<script>
+import School from './School.Vue';
+import Student from './Student.vue';
+
+export default {
+    name:'App',
+    components:{
+        // School:School,
+        // Student:Student
+        // 精简：
+        School,
+        Student
+
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 组件/单文件组件/School.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+    <button @click="showName">点我提示学校名</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "清华大学",
+      address: "北京",
+    };
+  },
+  methods: {
+    showName() {
+      alert(this.name);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
